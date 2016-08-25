58059a4183aad7a5b16c4bddc39023272f4d7f4c
rahul-soshte <rahul.soshte47@gmail.com>
2016-08-25 23:35:50 +0530

    Insertion sort main done

---
+++ insort.c
@@ -0,0 +1,35 @@
+//Insertion Sort
+#include<stdio.h>
+
+void insort(int a[],int n);
+
+int main()
+{ 
+int n;
+
+printf("Enter the number of elements to be sorted\n");
+scanf("%d",&n);
+
+int a[n];
+printf("Enter elements\n")
+for(i=0;i<n;i++)
+{
+printf("Element %d:",i+1);
+scanf("%d",&a[i]);
+}
+insort(c,n);
+printf("Sorted Elements are:");
+for(i=0;i<n;i++)
+{
+	printf("Element %d:",i+1);
+	printf("%d",a[i]);
+}
+return 0;
+
+}
+
+void insort(int a[],int n){
+	int a[];
+
+
+}
