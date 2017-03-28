dbcd7020fbe8f66103b8de19dbbd21bb1b6d4407
rahul-soshte <rahul.soshte47@gmail.com>
2017-03-29 03:24:13 +0530

    Tennis Tournament Structure

--- Divide and Conquer/tennis.c
+++ Divide and Conquer/tennis.c
@@ -1,1 +1,14 @@
 //TennisTournamentScheduling
+#include<stdio.h>
+
+int main()
+{
+	//Take input of no of players
+	//Even no of Players
+	//Divide the players into 2 sets
+	//Recursively till 2 people remain
+	//Each set takes n/2-1 days
+	
+
+
+}
