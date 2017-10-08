0ef50ba35d6b1ef7fd99027890d4c69412c8f97e
rahul-soshte <rahul.soshte47@gmail.com>
2017-10-08 16:03:40 +0530

    Webscraping quotes

--- webscrap/scrap_web_page.py
+++ webscrap/scrap_web_page.py
@@ -1,9 +1,13 @@
 from bs4 import BeautifulSoup
 import urllib2
 
-url = "https://stackoverflow.com/questions/45687034/how-to-change-layout-when-rotate"
+url = "https://www.goodreads.com/author/quotes/9810.Albert_Einstein"
 page = urllib2.urlopen(url)
 soup = BeautifulSoup(page.read())
+#print(soup.prettify())
+print soup.findAll("div", {"class": "quoteText"})
+#list = soup.findAll('div', attrs={'id':'quoteText'})
+#print len(list)
 
-for anchor in soup.find_all('a'):
-    print(anchor.get('href', '/')) 
+#for anchor in soup.find_all('a'):
+ #   print(anchor.get('href', '/')) 
