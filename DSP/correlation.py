1490f35275445bd22e3a42f93a7fe275c191db66
rahul-soshte <rahul.soshte47@gmail.com>
2018-07-30 21:01:46 +0530

    DSP

--- DSP/correlation.py
+++ DSP/correlation.py
@@ -1,21 +1,26 @@
 import numpy
 
-def start():
+def autocorrelation():
+	signaldata=[]
 	print("Enter the values of X(n)")
-	our_list = [] # create empty list
+	signaldata = [] # create empty list
 	number2 = int(input('Please enter the number of amplitudes: '))
 
 	for i in range(0, number2): # set up loop to run 10 times
 		number = int(input('Please enter a number: ')) # prompt user for number
-		our_list.append(number) # append to our_list
-	print(our_list)
-	origin = int (input("Origin is at position from the total length of 0-" + str(number2-1) ))
-	return our_list,origin
+		signaldata.append(number) # append to our_list
+	
+	origin = int (input("Origin is at position from the total length of 0-" + str(number2-1)+" "))
 
-def autocorrelateoutput(signaldata,origin):
-	signaldatafold=signaldata[::-1]
-	print(signaldatafold)
+	signaldatafold=foldsignal(signaldata,origin)
+
+	str1 = ','.join(str(e) for e in signaldata)
+	print("X(n)=["+str1+"]")
+	str2 = ','.join(str(e) for e in signaldatafold)
+	print("X(-n)=["+str2+"]")
+
 	i = len(signaldata)
+	
 	a = numpy.zeros(shape=(i,i))
 	y=[]
 
@@ -26,16 +31,15 @@ def autocorrelateoutput(signaldata,origin):
 		for j in range(0,i):
 			a[j][m]=signaldata[m]*signaldatafold[j]
 
-	for f in range(0,2*i-1):
-		for h in range(0,i):
-			for o in range(0,i):
-				if(h + o == f):
-					y[f] = y[f]+a[o][h]
+	for h in range(0,i):
+		for o in range(0,i):
+				y[h+o] += a[o][h]
 
-	return y
+	str3 = ','.join(str(e) for e in y)	
+			
+	print("Y(n)=["+str3+"]")			
 
-
-def crosscorrelateoutput():
+def crosscorrelation():
 	my_list = []
 	my_list2 = []
 
@@ -51,10 +55,18 @@ def crosscorrelateoutput():
 		number1 = int(input('Please enter a number: ')) # prompt user for number
 		my_list2.append(number1) # append to our_list
 
-	secondsignalfold = my_list2[::-1]
+	origin = int (input("Origin is at position for 2nd signal from the total length of 0-" + str(number3-1)+" "))
+	secondsignalfold=foldsignal(my_list2,origin)
+	# print("X(n)="+my_list)
+	str1 = ','.join(str(e) for e in my_list)
+	print("X(n)=["+str1+"]")
+	# 'X(n)=[{}]'.format(str1)
+	str2 = ','.join(str(e) for e in secondsignalfold)
+	print("h(-n)=["+str2+"]")
 
 	a = numpy.zeros(shape=(number3,number2))
-	print(a)
+
+
 	y=[]
 
 	for g in range(0,number2+number3-1):
@@ -63,14 +75,13 @@ def crosscorrelateoutput():
 	for m in range(0,number2):
 		for j in range(0,number3):
 			a[j][m]=my_list[m]*secondsignalfold[j]
+	
+	for h in range(0,number2):
+		for o in range(0,number3):
+			y[o+h] += a[o][h]
 
-	print(a)
-	for f in range(0,number2+number3-1):
-		for h in range(0,number2):
-			for o in range(0,number3):
-				if(h + o == f):
-					y[f] = y[f]+a[o][h]
-	return y
+	str3 = ','.join(str(e) for e in y)
+	print("Y(n)=["+str3+"]")
 
 def foldsignal(signaldata,origin):
 	my_list4=[]
@@ -85,13 +96,13 @@ def foldsignal(signaldata,origin):
 	my_list6=my_list4[::-1]
 	my_list7=my_list5[::-1]
 	mergedsignal=my_list7 + my_list6
-	print(mergedsignal)
+	return mergedsignal
 
 if __name__ == "__main__":
-	# signaldata, origin=start()
-	# print(signaldata)
-	# foldedsignal=foldsignal(signaldata,origin)
-	# output=autocorrelateoutput(signaldata,origin)
-	# print(output)
-	output2=crosscorrelateoutput()
-	print(output2)
+	
+	option = int(input('Type 1 for auto correlation and 2 for cross correlation '))
+	
+	if(option==1):
+		autocorrelation()
+	elif(option==2):
+		crosscorrelation()
