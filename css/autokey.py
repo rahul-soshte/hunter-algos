328850da85c1deeaab7cbbe3a7fa7bd125294acc
rahul-soshte <rahul.soshte47@gmail.com>
2018-08-18 00:28:35 +0530

    CSS

--- css/autokey.py
+++ css/autokey.py
@@ -6,11 +6,11 @@ if __name__ == "__main__":
 	keystream=[]
 	ciphertext=[]
 	autokeykey=7
+
 	text  = "the house is being sold tonight"
-	# text  = "hello"
+
 	for c in text:
 		if c==' ':
-			# val.append("blank")
 			pass
 		else:
 			val.append(ord(c)-ord('a'))
@@ -23,15 +23,27 @@ if __name__ == "__main__":
 			val2 = (val[i] + keystream[i]) % 26
 			ciphertext.append(val2)
 
-
 	for m in range(0, len(ciphertext)):
 		ciphertext[m]=chr( ciphertext[m] + ord('A'))
 
+	ciphertextstring=''.join(ciphertext)
+	
+	print(ciphertextstring)
 
-	# ciphertext=list(map(operator.add,val,keystream))
+	plaintext=[]
+	
+	for m in range(0, len(ciphertext)):
+		val3= ((ord(ciphertext[m])-ord('A')) - keystream[m]) % 26
+		plaintext.append(val3)
+
+	print(plaintext)
+	
+	for m in range(0, len(plaintext)):
+		plaintext[m]=chr( plaintext[m] + ord('A'))	
+
+	plaintextstring=''.join(plaintext)
+
+	print(plaintextstring)
 
 
 
-	print(val)
-	print(keystream)
-	print(ciphertext)
